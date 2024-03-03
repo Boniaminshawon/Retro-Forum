3cc4b34366373e07f54bb5ad2e7f9d3ace172356
boniaminshawon <shawonboni29@gmail.com>
2024-03-03 08:16:55 +0600

    complete the nav bar and banner section

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// set tool tip
+// const toolTipIcon = document.getElementsByClassName('icon');
+// console.log(toolTipIcon)
+// toolTipIcon.addEventListener("mouseover", showTooltip);
+// toolTipIcon.addEventListener("mouseout", hideTooltip);
+
+
+// function showTooltip() {
+//     const tooltip = document.getElementsByClassName('tooltip');
+//     console.log(tooltip)
+//   tooltip.classList.remove('hidden');
+//  }
+//  function hideTooltip() {
+//     const tooltip = document.getElementsByClassName('tooltip');
+//     console.log(tooltip)
+//     tooltip.classList.add('hidden')
+//  }
+
+
+
+
+
+
+
+
+
